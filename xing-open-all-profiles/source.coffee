012112b4660f6205671f3c6fbd12255102540f2a
Manuel Meurer <manuel@krautcomputing.com>
2017-01-25 16:00:46 +0100

    update "xing open all profiles" bookmarklet, xing ids can contain numbers

--- xing-open-all-profiles/source.coffee
+++ xing-open-all-profiles/source.coffee
@@ -1,4 +1,4 @@
 $('.search-result').each (i, el) ->
-  path = $('.name-page-link', el).attr('href').match(/\/profile\/[A-Za-z_]+/)
+  path = $('.name-page-link', el).attr('href').match(/\/profile\/[0-9A-Za-z_]+/)
   url = window.location.origin + path
   window.open url
--- xing-open-all-profiles/source.js
+++ xing-open-all-profiles/source.js
@@ -1,7 +1,7 @@
 (function() {
   $('.search-result').each(function(i, el) {
     var path, url;
-    path = $('.name-page-link', el).attr('href').match(/\/profile\/[A-Za-z_]+/);
+    path = $('.name-page-link', el).attr('href').match(/\/profile\/[0-9A-Za-z_]+/);
     url = window.location.origin + path;
     return window.open(url);
   });
